39d56bed03449daea56b0b364200e4558f589748
Vladres <vladres121@gmail.com>
2020-04-28 16:46:13 +0300

    fix bags with view articles)

--- ClientApp/src/app/Services/Articles/article.service.ts
+++ ClientApp/src/app/Services/Articles/article.service.ts
@@ -13,12 +13,12 @@ export class ArticleService {
       { Title : "Title4" , Text : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae amet cum enim." , imgSource : "https://i.pinimg.com/originals/0e/9e/88/0e9e8812f01f82650833264673bf51ed.jpg"},
       { Title : "Title5" , Text : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae amet cum enim." , imgSource : "https://i.pinimg.com/originals/0e/9e/88/0e9e8812f01f82650833264673bf51ed.jpg"},
       { Title : "Title6" , Text : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae amet cum enim. " , imgSource : "https://i.pinimg.com/originals/0e/9e/88/0e9e8812f01f82650833264673bf51ed.jpg"},
-      { Title : "Title1" , Text : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae amet cum enim." , imgSource : "https://i.pinimg.com/originals/0e/9e/88/0e9e8812f01f82650833264673bf51ed.jpg"},
-      { Title : "Title2" , Text : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae amet cum enim." , imgSource : "https://i.pinimg.com/originals/0e/9e/88/0e9e8812f01f82650833264673bf51ed.jpg"},
-      { Title : "Title3" , Text : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae amet cum enim. " , imgSource : "https://i.pinimg.com/originals/0e/9e/88/0e9e8812f01f82650833264673bf51ed.jpg"},
-      { Title : "Title4" , Text : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae amet cum enim." , imgSource : "https://i.pinimg.com/originals/0e/9e/88/0e9e8812f01f82650833264673bf51ed.jpg"},
-      { Title : "Title5" , Text : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae amet cum enim." , imgSource : "https://i.pinimg.com/originals/0e/9e/88/0e9e8812f01f82650833264673bf51ed.jpg"},
-      { Title : "Title6" , Text : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae amet cum enim. " , imgSource : "https://i.pinimg.com/originals/0e/9e/88/0e9e8812f01f82650833264673bf51ed.jpg"}
+      { Title : "Title7" , Text : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae amet cum enim." , imgSource : "https://i.pinimg.com/originals/0e/9e/88/0e9e8812f01f82650833264673bf51ed.jpg"},
+      { Title : "Title8" , Text : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae amet cum enim." , imgSource : "https://i.pinimg.com/originals/0e/9e/88/0e9e8812f01f82650833264673bf51ed.jpg"},
+      { Title : "Title9" , Text : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae amet cum enim. " , imgSource : "https://i.pinimg.com/originals/0e/9e/88/0e9e8812f01f82650833264673bf51ed.jpg"},
+      { Title : "Title10" , Text : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae amet cum enim." , imgSource : "https://i.pinimg.com/originals/0e/9e/88/0e9e8812f01f82650833264673bf51ed.jpg"},
+      { Title : "Title11" , Text : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae amet cum enim." , imgSource : "https://i.pinimg.com/originals/0e/9e/88/0e9e8812f01f82650833264673bf51ed.jpg"},
+      { Title : "Title12" , Text : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae amet cum enim. " , imgSource : "https://i.pinimg.com/originals/0e/9e/88/0e9e8812f01f82650833264673bf51ed.jpg"}
   ]
   
   constructor() { }
